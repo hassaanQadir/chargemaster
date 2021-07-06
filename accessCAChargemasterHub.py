5e9e64f7ae06eccfedee710b3016468b332b5e57
hassaanQadir <86531769+hassaanQadir@users.noreply.github.com>
2021-07-05 18:23:34 -0700

    Adds variables as labels

--- accessCAChargemasterHub.py
+++ accessCAChargemasterHub.py
@@ -1,5 +1,7 @@
 import requests
 
-r = requests.get('https://data.chhs.ca.gov/dataset/0c315f3b-fc3c-4998-bd79-4659616c834d/resource/95e415ee-5c11-40b9-b693-ff9af7985a94/download/chargemaster-cdm-2020.zip')
+targetURL = "https://data.chhs.ca.gov/dataset/0c315f3b-fc3c-4998-bd79-4659616c834d/resource/95e415ee-5c11-40b9-b693-ff9af7985a94/download/chargemaster-cdm-2020.zip"
 
-print(r.url)
+printThis = requests.get(targetURL)
+
+print(printThis.url)
