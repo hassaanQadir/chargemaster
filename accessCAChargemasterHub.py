8d3311808700f936b4218aa8eca88aee3493dfcb
hassaanQadir <86531769+hassaanQadir@users.noreply.github.com>
2021-07-05 18:28:09 -0700

    Adds ability to download the zip from the url

--- accessCAChargemasterHub.py
+++ accessCAChargemasterHub.py
@@ -5,3 +5,7 @@ targetURL = "https://data.chhs.ca.gov/dataset/0c315f3b-fc3c-4998-bd79-4659616c83
 printThis = requests.get(targetURL)
 
 print(printThis.url)
+
+savedFile = requests.get(targetURL)
+
+open(""C:\Users\Qadir\Major Projects\Coding\Chargemaster\theSavedFile.zip"", "w").write(savedFile.content)
