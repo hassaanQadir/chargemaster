ff18e319858e46590f56f6b03570315e6af4d31e
hassaanQadir <86531769+hassaanQadir@users.noreply.github.com>
2021-07-06 17:33:51 -0700

    Adds ZipFile and extracts downloaded zip file

--- accessCAChargemasterHub.py
+++ accessCAChargemasterHub.py
@@ -1,15 +1,19 @@
 import requests
+from zipfile import ZipFile
 
 targetURL = "https://data.chhs.ca.gov/dataset/0c315f3b-fc3c-4998-bd79-4659616c834d/resource/95e415ee-5c11-40b9-b693-ff9af7985a94/download/chargemaster-cdm-2020.zip"
 
-savedFile = requests.get(targetURL, stream = True)
+downloadedFile = requests.get(targetURL, stream = True)
 
-print(savedFile.url)
+print(downloadedFile.url)
 
-with open("C:\\Users\\Qadir\\Major Projects\\Coding\\Chargemaster\\theSavedFile.zip", "wb") as savedZip:
+with open("C:\\Users\\Qadir\\Major Projects\\Coding\\Chargemaster\\CAChargemasterSavedFile.zip", "wb") as savedZip:
 
 	for chunk in savedFile.iter_content(chunk_size = 1024):
 
 		if chunk:
-
 			savedZip.write(chunk)
+			
+with ZipFile('savedZip', 'r') as targetZip:
+   # Extract all the contents of zip file in current directory
+   targetZip.extractall()			
