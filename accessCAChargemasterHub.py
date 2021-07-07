e9234ab520c5f503f3ebdfa55467ba54a78ad670
hassaanQadir <86531769+hassaanQadir@users.noreply.github.com>
2021-07-06 18:32:50 -0700

    Adds comments framing out my next steps

--- accessCAChargemasterHub.py
+++ accessCAChargemasterHub.py
@@ -1,5 +1,6 @@
 import requests
 from zipfile import ZipFile
+import pandas as pd
 
 targetURL = "https://data.chhs.ca.gov/dataset/0c315f3b-fc3c-4998-bd79-4659616c834d/resource/95e415ee-5c11-40b9-b693-ff9af7985a94/download/chargemaster-cdm-2020.zip"
 
@@ -16,4 +17,16 @@ with open("C:\\Users\\Qadir\\Major Projects\\Coding\\Chargemaster\\Runtime\\CACh
 			
 with ZipFile("C:\\Users\\Qadir\\Major Projects\\Coding\\Chargemaster\\Runtime\\CAChargemasterSavedFile.zip", "r") as targetZip:
    # Extract all the contents of zip file in current directory
-   targetZip.extractall()			
+   targetZip.extractall()	
+	
+#iterate through the extracted folder the following{
+#open each child folder and append the name of the child to the grandchild file
+#copy grandchild file and bring to child-level of the original folder
+#delete the child folder}
+#then iterate through the new child files and read and print their excel files into one huge list
+
+
+#base code for reading and printing excel files{
+#allCAChargemasters = pd.read_excel (r"C:\\Users\\Qadir\\Major Projects\\Coding\\Chargemaster\\Runtime\\Chargemaster CDM 2020")
+#print (allCAChargemasters)
+#}
